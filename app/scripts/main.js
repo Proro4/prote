02968832ea0f764ab29e04cf88f186d4c95f206f
DenisDev <proro4@gmail.com>
2018-10-16 09:32:25 +0300

    add pages news, news-detail, catalog, product

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -36,6 +36,38 @@ $(document).ready(function () {
         $(this).addClass('active');
     })
 
+    // mob menu button click
+
+    $('.mob-menu-but').on('click',function () {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('.header-mob').removeClass('active');
+            $(this).addClass('close');
+        }else{
+            $(this).addClass('active');
+            $('.header-mob').addClass('active');
+            $(this).removeClass('close');
+
+        }
+    })
+    // catalog list open
+    $('.catalog__menu__block__title').on('click',function () {
+        if($(this).parent().hasClass('active')){
+            $(this).parent().removeClass('active');
+        }else{
+            $('.catalog__menu__block').removeClass('active');
+            $(this).parent().addClass('active');
+        }
+    });
+    $('.catalog__menu__block__list p').on('click',function () {
+        if($(this).parent().find('ul').hasClass('active')){
+            $(this).removeClass('active');
+            $(this).parent().find('ul').removeClass('active');
+        }else{
+            $(this).parent().find('ul').addClass('active')
+            $(this).addClass('active');
+        }
+    })
 });
 $(window).resize(function () {
     sliderTopPosLeft();
